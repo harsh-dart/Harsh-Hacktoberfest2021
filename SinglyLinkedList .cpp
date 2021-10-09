2acab7349b59415ae50132b2408e0bb35da88860
Himanshu Patel <himanshu.patel_cs19@gla.ac.in>
2021-10-09 13:25:41 +0530

    SinglyLinkedList

---
+++ SinglyLinkedList .cpp
@@ -0,0 +1,16 @@
+package Linked_List;
+
+public class SinglyLinkedList {
+    private int data;
+    private SinglyLinkedList next;
+    public SinglyLinkedList(int data)
+    {
+        this.data=data;
+        this.next=null;
+    }
+
+    public static void main(String[] args) {
+     SinglyLinkedList head;
+
+    }
+}
