ebdaed0efbf2672d0aa3b1b532aee24a51dfaa82
Harsh Tripathi <89039023+harsh-dart@users.noreply.github.com>
2021-10-18 00:07:05 +0530

    Update DoublyLinkedList.java

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -55,9 +55,7 @@ public class DoublyLinkedList {
         } else
             System.out.println("Empty List");
     }
-     public void deleteFromLast() {
-    }
-
+    
     public void displayFromStart() {
         if (!isEmpty()) {
             Node temp = head;
